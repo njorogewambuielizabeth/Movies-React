6b5ffc0fc078219cfc61b1730553f9e0fc7a30d5
njorogewambuielizabeth <njorogewambuielizabeth@gmail.com>
2023-07-27 17:02:55 +0300

    fetched movies from an API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import FetchMovies from './Components/GetMovies';
+import DisplayMovies from './Components/GetMovies/Carousel';
+import Navbar from './Components/GetMovies/NavigationBar';
+import MovieDetails from './Components/GetMovies/MovieDetails';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <DisplayMovies/>
+      <Routes>
+        <Route path='/' element={<FetchMovies />}/>
+        <Route path='/movies/:movieId' element={<MovieDetails />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
+
+
 
 export default App;
---
+++ src/Components/GetMovies/index.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { getMovies } from "../../Utilities/utilities";
+import ImageContainer from "../../Atoms/ImageContainer";
+import "./style.css";
+import CategoryFilter from "./GetCategories/movieFilter";
+import MovieDetails from "./MovieDetails";
+
+const FetchMovies = ({ searchResults }) => {
+  const [movies, setMovies] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [selectedCategoryName, setSelectedCategoryName] = useState("All");
+  const [selectedMovie, setSelectedMovie] = useState(null); // State to store the selected movie for displaying details
+
+  useEffect(() => {
+    (async () => {
+      const movies = await getMovies();
+      setMovies(movies.results);
+    })();
+  }, []);
+
+  const handleCategoryChange = (categoryId, categoryName) => {
+    setSelectedCategory(categoryId);
+    setSelectedCategoryName(categoryName);
+  };
+
+  const handleMovieClick = (movieId) => {
+    setSelectedMovie(movieId); 
+  };
+
+  const handleCloseMovieDetails = () => {
+    setSelectedMovie(null); 
+  };
+
+  const filteredMovies =
+    selectedCategory === "all"
+      ? movies
+      : movies.filter((movie) => movie.genre_ids.includes(parseInt(selectedCategory)));
+
+  const moviesToDisplay = searchResults && searchResults.length > 0 ? searchResults : filteredMovies;
+
+  return (
+    <div>
+      {selectedCategoryName !== "All" && <h1>{selectedCategoryName} Movies</h1>}
+
+      <CategoryFilter
+        selectedCategory={selectedCategory}
+        handleCategoryChange={handleCategoryChange}
+      />
+
+      <div className="movies">
+        {moviesToDisplay.map((item) => (
+          <ImageContainer
+            props={item}
+            key={item.id}
+            onClick={() => handleMovieClick(item.id)} 
+          />
+        ))}
+      </div>
+
+      {selectedMovie && (
+        <MovieDetails movieId={selectedMovie} onClose={handleCloseMovieDetails} />
+      )}
+    </div>
+  );
+};
+
+export default FetchMovies;
+
